a7a5945e5d541d69e4bceb2f388b1688baa4e0af
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2022-05-22 03:42:30 +0100

    clean

--- minting-dapp/src/scripts/main.tsx
+++ minting-dapp/src/scripts/main.tsx
@@ -4,5 +4,6 @@ import ReactDOM from 'react-dom';
 import Dapp from './react/Dapp';
 
 document.addEventListener('DOMContentLoaded', async () => {
+  console.log('rendering from react')
   ReactDOM.render(<Dapp />, document.getElementById('minting-dapp'));
 });
